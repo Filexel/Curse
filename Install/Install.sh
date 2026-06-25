7bcbf6520057dfd967d213299ef6015d2c4e1525
Filexel <mhehopm34@mail.ru>
2026-06-25 13:40:09 +0500

    Update Install.sh

--- Install/Install.sh
+++ Install/Install.sh
@@ -3,8 +3,8 @@
 echo "Установка Curse..."
 
 # Скачиваем файлы
-wget https://raw.githubusercontent.com/Filexel/Curse/main/Utils/Exit.sh
-wget https://raw.githubusercontent.com/Filexel/Curse/main/Utils/Start.sh
+wget https://raw.githubusercontent.com/Filexel/Curse-Android/main/Utils/Exit.sh
+wget https://raw.githubusercontent.com/Filexel/Curse-Android/main/Utils/Start.sh
 
 # Копируем в bin
 cp Start.sh /usr/bin/curse-start
@@ -19,4 +19,4 @@ echo "curse-start — Запустить Curse Shell"
 echo "curse-exit — Выйти"
 
 
-3.0
+4.0
